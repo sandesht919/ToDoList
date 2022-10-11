81c6ca37ed69c7b8e2af434e49e52036e252d16f
Sandesh <114343891+sandesht919@users.noreply.github.com>
2022-10-11 22:46:37 +0545

    Third Commit

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
-console.log(date)
 const app = express();
+
 app.set("view engine", "ejs"); 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
